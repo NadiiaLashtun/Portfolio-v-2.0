1139bd8c28491c737deb96b3180483b23a0e6b3d
FlorSmile <flor.smile1@i.ua>
2025-02-14 17:17:23 +0200

    Add responsiveness to the Contact page

--- src/components/Pages/ContactsPage.jsx
+++ src/components/Pages/ContactsPage.jsx
@@ -7,14 +7,12 @@ function ContactsPage() {
   return (
     <section className='relative px-16 pt-16 bg-brand-color -z-10'>
       <div className='w-1/3 h-[1px] absolute top-[60px] left-1/2 -translate-x-1/2 bg-black'></div>
-      <div className='flex items-start pt-7'>
-        <div className='grow w-full'>
-          <div className='translate-x-[55%]'>
-            <SocialList color='text-black' />
-          </div>
-        </div>
 
-        <div className='grow-0 flex flex-col gap-4 align-end mb-20'>
+      <div className='flex justify-center pt-7'>
+        <SocialList color='text-black' />
+      </div>
+      <div className='flex justify-center lg:justify-end pt-12 lg:pt-0'>
+        <div className='flex flex-col gap-4 mb-20 '>
           <div className='flex items-center gap-3'>
             <img src={Phone} alt='Phone' />
             <a href='tel:+380982895423'>+38(098)-289-54-23</a>
@@ -27,7 +25,8 @@ function ContactsPage() {
           </div>
         </div>
       </div>
-      <h2 className='font-Montserrat-Alternates font-bold text-[180px] text-white text-left'>
+
+      <h2 className='font-Montserrat-Alternates font-bold text-[120px] md:text-[180px] text-white text-left'>
         Contacts
       </h2>
       <BackgroundText text='contacts' left='10%' bottom='0' />
@@ -36,3 +35,5 @@ function ContactsPage() {
 }
 
 export default ContactsPage;
+
+/*translate-x-[60%]*/
