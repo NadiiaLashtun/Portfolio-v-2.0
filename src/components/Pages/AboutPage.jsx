a460ae3703da28fd6cad3442114cdb09ad1bdc8b
NadiiaLashtun <flor.smile11@gmail.com>
2025-09-05 18:44:12 +0200

    Add information to About section. Fix responsiveness

--- src/components/Pages/AboutPage.jsx
+++ src/components/Pages/AboutPage.jsx
@@ -1,29 +1,30 @@
-import Button from "../UI/Button";
-import AboutImg from "../../assets/about-img.png";
-import BackgroundText from "../UI/BackgroundText";
-import BackgroundSquare from "../UI/BackgroundSquare";
-import ArrowDown from "../../assets/arrow-down.svg";
+import Button from '../UI/Button';
+import AboutImg from '../../assets/about-img.png';
+import BackgroundText from '../UI/BackgroundText';
+import BackgroundSquare from '../UI/BackgroundSquare';
+import ArrowDown from '../../assets/arrow-down.svg';
 
 function AboutPage() {
   return (
     <section
-      className='flex flex-col-reverse md:flex-row gap-20 max-h-full p-[30px] sm:p-[60px] lg:p-[120px] relative'
+      className='flex flex-col-reverse md:flex-row gap-14 lg:gap-20 max-h-full p-[30px] sm:p-[60px] lg:py-[120px] relative'
       id='about-page'
     >
-      <div className='absolute top-[75%] sm:top-[50%] md:top-[6%] left-0 w-full md:w-[50%] h-[25%] md:h-[50%]'>
+      <div className='absolute top-[54%] sm:top-[50%] md:top-[4%] left-0 w-full md:w-[50%] h-[25%] md:h-[50%]'>
         <BackgroundSquare />
       </div>
 
-      <div className='relative w-full md:w-1/2 flex flex-col gap-9 text-left pl-12 md:pl-24 mx-auto'>
+      <div className='relative w-full md:w-1/2 flex flex-col gap-9 text-left pl-12 lg:pl-24 p-8 mx-auto'>
         <img
           src={ArrowDown}
-          className='absolute h-2/3 left-[-2%] bottom-[40%] top-1/2 -translate-y-1/2'
+          className='absolute h-1/2 left-[-2%] bottom-[40%] top-1/2 -translate-y-1/2'
           alt='Arrow'
         />
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          tristique aliquet est, quis aliquam eros volutdf. Proin accumsan augue
-          ac libero rhoncus, vel mattis dolor dictum.
+          I am a Frontend Developer with a strong IT background, experienced in
+          creating clean, maintainable, and high-performing web applications. My
+          goal is to deliver scalable and impactful solutions that combine clean
+          design with high functionality.
         </p>
 
         <img
@@ -46,18 +47,34 @@ function AboutPage() {
           top='-10%'
           left='55%'
         />
-        <div className='flex gap-10 text-left'>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            tristique aliquet est, quis aliquam eros volutdf. Proin accumsan
-            augue ac libero rhoncus, vel mattis dolor dictum. rem ipsum dolor
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            tristique aliquet est, quis aliquam eros volutdf. Proin accumsan
-            augue ac libero rhoncus, vel mattis dolor dictum. rem ipsum dolor
-          </p>
-          <p className='text-brand-color text-[110px]'>/</p>
+        <div className='flex gap-4 lg:gap-8 text-left'>
+          <div className='flex gap-6 lg:gap-8 flex-wrap'>
+            <div className='flex-1 min-w-44'>
+              <h3 className='bg-text-white-color text-brand-color my-4'>
+                What I do:
+              </h3>
+              <ul className='space-y-5 lg:space-y-5 list-disc list-outside border-l-[1px] marker:text-brand-color border-brand-color pl-[14px]'>
+                <li className=''>
+                  Build responsive and user-friendly interfaces
+                </li>
+                <li>Work with UI/UX tools (Figma, Canva) </li>
+                <li>Collaborate in teams with Jira</li>
+                <li>Use version control (Git)</li>
+              </ul>
+            </div>
+            <div className='flex-1 min-w-44'>
+              <h3 className='bg-text-white-color text-brand-color my-4'>
+                I’ve completed:
+              </h3>
+              <ul className='space-y-5 lg:space-y-5 list-disc list-outside border-l-[1px] marker:text-brand-color border-brand-color pl-[14px]'>
+                <li>Several international team projects at Chingu</li>
+                <li>Specialized courses (SheCodes, Genius)</li>
+                <li>I speak English (B1), Czech (A2)</li>
+                <li>Open to remote opportunities</li>
+              </ul>
+            </div>
+          </div>
+          <p className='text-brand-color text-[110px] flex-grow-0'>/</p>
         </div>
       </div>
     </section>
