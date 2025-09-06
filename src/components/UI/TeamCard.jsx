747050c5e239eaea7dbaf3bb8928641ad0e5ed72
NadiiaLashtun <flor.smile11@gmail.com>
2025-09-06 17:17:49 +0200

    Fix team project cards scaling

--- src/components/UI/TeamCard.jsx
+++ src/components/UI/TeamCard.jsx
@@ -1,24 +1,34 @@
-import Button from "./Button";
-import HorLine from "../../assets/hor-line.svg";
+import Button from './Button';
+import HorLine from '../../assets/hor-line.svg';
 
 function TeamCard({ title, img, descr, netlify, github }) {
   return (
     <div className='flex flex-col items-center w-[90%] sm:w-1/3'>
-      <div className='relative mb-6 max-w-[433px] max-h-[326px] border border-gray-800 shadow-lg cursor-pointer'>
-        <a href={netlify} target='_blank'>
+      <div className='relative mb-6 max-w-[433px] max-h-[326px] shadow-lg cursor-pointer'>
+        <a
+          href={netlify}
+          target='_blank'
+        >
           <img
             src={img}
-            className='w-full h-full object-cover transition-transform duration-700 transform hover:scale-125 hover:brightness-105'
+            className='w-full h-full object-cover transform duration-500 hover:scale-105 border border-text-color hover:brightness-110 shadow-lg'
             alt='Project image'
           />
         </a>
       </div>
       <div className='mb-5 mx-auto'>
-        <a href={github} target='_blank'>
+        <a
+          href={github}
+          target='_blank'
+        >
           <Button caption={title} />
         </a>
       </div>
-      <img src={HorLine} className='mb-9' alt='Line' />
+      <img
+        src={HorLine}
+        className='mb-9'
+        alt='Line'
+      />
       <p>{descr}</p>
     </div>
   );
