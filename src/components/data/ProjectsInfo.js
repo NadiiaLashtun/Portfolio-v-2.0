462ea12079f0102dfc12f5f5f4210e9df7013e6e
NadiiaLashtun <flor.smile11@gmail.com>
2025-09-08 12:29:45 +0200

    Add description to Color Compass project

--- src/components/data/ProjectsInfo.js
+++ src/components/data/ProjectsInfo.js
@@ -1,52 +1,52 @@
 export const soloProjectsInfo = [
   {
-    year: "2025",
-    name: "Miami-condo-kings",
-    img: "/src/assets/portfolio-projects/miami-condo-kings.jpg",
-    netlify: "https://miami-condo-kings.netlify.app/",
-    github: "https://github.com/NadiiaLashtun/Miami-condo-kings",
+    year: '2025',
+    name: 'Miami-condo-kings',
+    img: '/src/assets/portfolio-projects/miami-condo-kings.jpg',
+    netlify: 'https://miami-condo-kings.netlify.app/',
+    github: 'https://github.com/NadiiaLashtun/Miami-condo-kings',
     description:
-      "Premium real estate company specializing in new buildings. Offers property sales, rentals, and investment opportunities in the high-end market. Provides expert knowledge and direct connections with developers.",
+      'Premium real estate company specializing in new buildings. Offers property sales, rentals, and investment opportunities in the high-end market. Provides expert knowledge and direct connections with developers.',
     flip: false,
   },
   {
-    year: "2025",
-    name: "Treveland",
-    img: "/src/assets/portfolio-projects/treveland.jpg",
-    netlify: "https://trevland-project.netlify.app/",
-    github: "https://github.com/NadiiaLashtun/Trevland",
+    year: '2025',
+    name: 'Treveland',
+    img: '/src/assets/portfolio-projects/treveland.jpg',
+    netlify: 'https://trevland-project.netlify.app/',
+    github: 'https://github.com/NadiiaLashtun/Trevland',
     description:
-      "Travel company specializing in worldwide tours and adventures. Offers personalized trips, group tours, and unique travel experiences. Helps travelers explore new destinations with comfort and reliability.",
+      'Travel company specializing in worldwide tours and adventures. Offers personalized trips, group tours, and unique travel experiences. Helps travelers explore new destinations with comfort and reliability.',
     flip: true,
   },
 
   {
-    year: "2024",
-    name: "Organik",
-    img: "/src/assets/portfolio-projects/organik.jpg",
-    netlify: "https://organik-nature.netlify.app/",
-    github: "https://github.com/NadiiaLashtun/Organik",
+    year: '2024',
+    name: 'Organik',
+    img: '/src/assets/portfolio-projects/organik.jpg',
+    netlify: 'https://organik-nature.netlify.app/',
+    github: 'https://github.com/NadiiaLashtun/Organik',
     description:
-      "Organic products store focused on healthy and natural living. Offers fresh, eco-friendly food and everyday essentials. Supports sustainable farming and environmentally conscious choices.",
+      'Organic products store focused on healthy and natural living. Offers fresh, eco-friendly food and everyday essentials. Supports sustainable farming and environmentally conscious choices.',
     flip: false,
   },
 ];
 
 export const teamProjectsInfo = [
   {
-    name: "Color Compass",
-    img: "/src/assets/color-compass.jpg",
-    netlify: "https://color-compass-v49-team-16.netlify.app/",
-    github: "https://github.com/chingu-voyages/v49-tier2-team-16",
+    name: 'Color Compass',
+    img: '/src/assets/color-compass.jpg',
+    netlify: 'https://color-compass-v49-team-16.netlify.app/',
+    github: 'https://github.com/chingu-voyages/v49-tier2-team-16',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tristique aliquet est, quis aliquam eros volutdf. Proin accums ",
+      'Color Compass – Your AI-Powered Color Assistant. Pick a base color and get harmonious complementary palettes with smart suggestions for design, fashion, decor, or any creative project.',
   },
   {
-    name: "Dino World",
-    img: "/src/assets/dino-world.jpg",
-    netlify: "https://dino-worlds.netlify.app/",
-    github: "https://github.com/chingu-voyages/v48-tier1-team-02",
+    name: 'Dino World',
+    img: '/src/assets/dino-world.jpg',
+    netlify: 'https://dino-worlds.netlify.app/',
+    github: 'https://github.com/chingu-voyages/v48-tier1-team-02',
     description:
-      "Dinoworld is a webapp provides a great all-in-one learning tool for people of all ages and develops the knowledge and history you need to know about the dinosaurs.",
+      'Dinoworld is a webapp provides a great all-in-one learning tool for people of all ages and develops the knowledge and history you need to know about the dinosaurs.',
   },
 ];
