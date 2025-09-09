a0a11ece3ba97d23b9a805a59a8d101291feb168
NadiiaLashtun <flor.smile11@gmail.com>
2025-09-09 18:40:20 +0200

    Add animation to project cards

--- src/components/UI/ProjectCard.jsx
+++ src/components/UI/ProjectCard.jsx
@@ -1,5 +1,5 @@
-import Button from './Button';
-import VertLine from '../../assets/vert-line.svg';
+import Button from "./Button";
+import VertLine from "../../assets/vert-line.svg";
 
 function ProjectCard({
   projectName,
@@ -12,7 +12,7 @@ function ProjectCard({
   return (
     <div
       className={`flex flex-col md:flex-row gap-10 md:gap-20 lg:gap-40 h-fit ${
-        flip ? 'md:flex-row-reverse' : 'md:flex-row '
+        flip ? "md:flex-row-reverse" : "md:flex-row "
       }`}
     >
       {/* Card info */}
@@ -22,13 +22,13 @@ function ProjectCard({
           after:content-[''] after:absolute after:top-0 after:w-[10px] after:h-[10px] after:bg-black after:rounded-full
           ${
             flip
-              ? 'pl-12 md:pl-0 before:left-0 md:before:-left-10 lg:before:-left-20 after:left-[-4.2px] md:after:-left-[43.2px] lg:after:-left-[83.2px] items-end min-h-[400px] '
-              : 'pr-12 md:pr-0 before:right-0 md:before:-right-10 lg:before:-right-20 after:right-[-3.8px] md:after:-right-[44.2px] lg:after:-right-[84.2px] min-h-[260px] md:min-h-[340px]'
+              ? "pl-12 md:pl-0 before:left-0 md:before:-left-10 lg:before:-left-20 after:left-[-4.2px] md:after:-left-[43.2px] lg:after:-left-[83.2px] items-end min-h-[400px] "
+              : "pr-12 md:pr-0 before:right-0 md:before:-right-10 lg:before:-right-20 after:right-[-3.8px] md:after:-right-[44.2px] lg:after:-right-[84.2px] min-h-[260px] md:min-h-[340px]"
           }`}
       >
         <div
           className={`flex flex-col gap-5 ${
-            flip ? ' items-start' : 'items-end'
+            flip ? " items-start" : "items-end"
           }`}
         >
           <a
@@ -44,7 +44,7 @@ function ProjectCard({
           <h4 className='mb-4 font-bold text-2xl text-brand-color'>
             {projectName}
           </h4>
-          <p className='text-justify'>{descr}</p>{' '}
+          <p className='text-justify'>{descr}</p>{" "}
         </div>
 
         {/* Line */}
@@ -66,7 +66,7 @@ function ProjectCard({
         <img
           src={img}
           alt='Project'
-          className='w-full object-contain border border-text-color shadow-lg'
+          className='w-full object-contain border border-text-color shadow-lg transform duration-500 hover:scale-105 hover:brightness-110'
         />
       </a>
     </div>
