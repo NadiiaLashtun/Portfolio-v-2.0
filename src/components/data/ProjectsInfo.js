96ca89e067cf38dd8d7dfafd03c600425b99dbb7
NadiiaLashtun <flor.smile11@gmail.com>
2025-10-22 13:43:23 +0200

    Add info about new team projects

--- src/components/data/ProjectsInfo.js
+++ src/components/data/ProjectsInfo.js
@@ -1,58 +1,76 @@
-import MiamiCondoKings from "/src/assets/portfolio-projects/miami-condo-kings.jpg";
-import Trevland from "/src/assets/portfolio-projects/treveland.jpg";
-import Organik from "/src/assets/portfolio-projects/organik.jpg";
-import ColorCompass from "/src/assets/color-compass.jpg";
-import DinoWorld from "/src/assets/dino-world.jpg";
+import MiamiCondoKings from '/src/assets/portfolio-projects/miami-condo-kings.jpg';
+import Trevland from '/src/assets/portfolio-projects/treveland.jpg';
+import Organik from '/src/assets/portfolio-projects/organik.jpg';
+import ColorCompass from '/src/assets/color-compass.jpg';
+import DinoWorld from '/src/assets/dino-world.jpg';
+import PRStatusBoard from '/src/assets/pr-status-board.jpg';
+import SaveBees from '/src/assets/bee.jpg';
 
 export const soloProjectsInfo = [
   {
-    year: "2025",
-    name: "Miami-condo-kings",
+    year: '2025',
+    name: 'Miami-condo-kings',
     img: MiamiCondoKings,
-    netlify: "https://miami-condo-kings.netlify.app/",
-    github: "https://github.com/NadiiaLashtun/Miami-condo-kings",
+    netlify: 'https://miami-condo-kings.netlify.app/',
+    github: 'https://github.com/NadiiaLashtun/Miami-condo-kings',
     description:
-      "Premium real estate company specializing in new buildings. Offers property sales, rentals, and investment opportunities in the high-end market. Provides expert knowledge and direct connections with developers.",
+      'Premium real estate company specializing in new buildings. Offers property sales, rentals, and investment opportunities in the high-end market. Provides expert knowledge and direct connections with developers.',
     flip: false,
   },
   {
-    year: "2025",
-    name: "Treveland",
+    year: '2025',
+    name: 'Treveland',
     img: Trevland,
-    netlify: "https://trevland-project.netlify.app/",
-    github: "https://github.com/NadiiaLashtun/Trevland",
+    netlify: 'https://trevland-project.netlify.app/',
+    github: 'https://github.com/NadiiaLashtun/Trevland',
     description:
-      "Travel company specializing in worldwide tours and adventures. Offers personalized trips, group tours, and unique travel experiences. Helps travelers explore new destinations with comfort and reliability.",
+      'Travel company specializing in worldwide tours and adventures. Offers personalized trips, group tours, and unique travel experiences. Helps travelers explore new destinations with comfort and reliability.',
     flip: true,
   },
 
   {
-    year: "2024",
-    name: "Organik",
+    year: '2024',
+    name: 'Organik',
     img: Organik,
-    netlify: "https://organik-nature.netlify.app/",
-    github: "https://github.com/NadiiaLashtun/Organik",
+    netlify: 'https://organik-nature.netlify.app/',
+    github: 'https://github.com/NadiiaLashtun/Organik',
     description:
-      "Organic products store focused on healthy and natural living. Offers fresh, eco-friendly food and everyday essentials. Supports sustainable farming and environmentally conscious choices.",
+      'Organic products store focused on healthy and natural living. Offers fresh, eco-friendly food and everyday essentials. Supports sustainable farming and environmentally conscious choices.',
     flip: false,
   },
 ];
 
 export const teamProjectsInfo = [
   {
-    name: "Color Compass",
+    name: 'Color Compass',
     img: ColorCompass,
-    netlify: "https://color-compass-v49-team-16.netlify.app/",
-    github: "https://github.com/chingu-voyages/v49-tier2-team-16",
+    netlify: 'https://color-compass-v49-team-16.netlify.app/',
+    github: 'https://github.com/chingu-voyages/v49-tier2-team-16',
     description:
-      "Your AI-Powered Color Assistant. Pick a base color and get harmonious complementary palettes with smart suggestions for design, fashion, decor, or any creative project.",
+      'Your AI-Powered Color Assistant. Pick a base color and get harmonious complementary palettes with smart suggestions for design, fashion, decor, or any creative project.',
   },
   {
-    name: "Dino World",
+    name: 'Dino World',
     img: DinoWorld,
-    netlify: "https://dino-worlds.netlify.app/",
-    github: "https://github.com/chingu-voyages/v48-tier1-team-02",
+    netlify: 'https://dino-worlds.netlify.app/',
+    github: 'https://github.com/chingu-voyages/v48-tier1-team-02',
     description:
-      "Dinoworld is a webapp provides a great all-in-one learning tool for people of all ages and develops the knowledge and history you need to know about the dinosaurs.",
+      'Dinoworld is a webapp provides a great all-in-one learning tool for people of all ages and develops the knowledge and history you need to know about the dinosaurs.',
+  },
+  {
+    name: 'Status Board',
+    img: PRStatusBoard,
+    netlify: 'https://github-prs-board.netlify.app/',
+    github: 'https://github.com/chingu-voyages/V57-tier2-team-20',
+    description:
+      'PR Status Board helps teams efficiently track and manage GitHub Pull Requests. It visualizes PR status, making reviews and team responsibilities easy to follow.',
+  },
+  {
+    name: 'Save Bees',
+    img: SaveBees,
+    netlify: 'https://the-bee-saving-project.onrender.com/',
+    github: 'https://github.com/KseniiaRiabova/the-bee-saving-project',
+    description:
+      'This web app helps protect wild bees by letting users report bees in need and find local rescue opportunities. It offers a personal dashboard, data insights, and tips for bee conservation.',
   },
 ];
